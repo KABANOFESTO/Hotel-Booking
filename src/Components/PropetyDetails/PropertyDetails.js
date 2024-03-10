68ec5e30dd4dedc7c3a388e59ee6967f81bc4ee7
KABANOFESTO <kabanofesto1@gmail.com>
2024-03-10 17:11:20 +0200

    ch-addDesc-mapComponent

--- src/Components/PropetyDetails/PropertyDetails.js
+++ src/Components/PropetyDetails/PropertyDetails.js
@@ -6,6 +6,7 @@ import { getPropertyDetails } from '../../Store/PropertyDetails/propertyDetails-
 import PropertyImg from './PropertyImg';
 import PropertyAmenities from './PropertyAmenities';
 import BookingForm from './BookingForm';
+import MapComponent from './MapComponent';
 
 const PropertyDetails = () => {
     const dispatch = useDispatch();
@@ -26,6 +27,7 @@ const PropertyDetails = () => {
         amenities,
         price,
         currentBookings,
+        extraInfo,
     } = propertydetails;
 
     return (
@@ -50,14 +52,32 @@ const PropertyDetails = () => {
                             <PropertyAmenities amenities={amenities} />
                             <br /><br />
                         </div>
-                        <BookingForm
-                            propertyId={id}
-                            price={price}
-                            propertyName={propertyName}
-                            address={address}
-                            maximumGuest={maximumGuest}
-                            currentBookings={currentBookings}
-                        />
+                        <div className='property-payment col-md-4 col-sm-12 col-12'>
+                            <BookingForm
+                                propertyId={id}
+                                price={price}
+                                propertyName={propertyName}
+                                address={address}
+                                maximumGuest={maximumGuest}
+                                currentBookings={currentBookings}
+                            />
+                        </div>
+                    </div>
+
+                    <hr />
+                    <div className='property-map'>
+                        <div className='map-image-exinfo-container row'>
+                            <div className='map-image-container col-md-6 col-sm-12 col-12'>
+                                <h2 className='map-header'>Where you will be</h2>
+                                <MapComponent
+                                    address={address}
+                                />
+                            </div>
+                            <div className='extra-info col-md-6 col-sm-12 col-12'>
+                                <h2 className='extra-heading'>Extra Info</h2>
+                                <p className='extra-description'>{extraInfo}</p>
+                            </div>
+                        </div>
                     </div>
                 </>
             )
@@ -65,5 +85,4 @@ const PropertyDetails = () => {
         </div>
     )
 }
-
 export default PropertyDetails
