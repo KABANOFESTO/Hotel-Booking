3d92e09c325aca2d0f0ae40aaeb5259fb381558b
KABANOFESTO <kabanofesto1@gmail.com>
2024-03-19 12:48:24 +0200

    ch-editProfile

--- src/App.js
+++ src/App.js
@@ -7,10 +7,13 @@ import { Flip, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Use BrowserRouter instead of createBrowserRouter
+import { BrowserRouter as Router, Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'; // Use BrowserRouter instead of createBrowserRouter
 import { currentUser } from "./Store/User/user-action";
 import { userActions } from './Store/User/user-slice';
 import Signup from './Components/User/Signup';
+import Profile from './Components/User/Profile';
+import EditProfile from './Components/User/EditProfile';
+import UpdatePassword from './Components/User/UpdatePassword';
 
 function App() {
   const dispatch = useDispatch();
@@ -22,19 +25,23 @@ function App() {
     }
     dispatch(currentUser());
   }, [errors, dispatch]);
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Main />} id='main' exact>
+        <Route id='home' index element={<PropertyList />} exact />
+        <Route id='PropertyDetails' path="propertylist/:id" element={<PropertyDetails />} exact />
+        <Route id='login' path="login" element={<Login />} />
+        <Route id='signup' path="signup" element={<Signup />} />
+        <Route id='profile' path='profile' element={<Profile />} />
+        <Route id='editprofile' path='editprofile' element={<EditProfile />} />
+        <Route id='updatepassoword' path='user/updatepassword' element={<UpdatePassword/>}/>
+      </Route>
+    )
+  );
 
   return (
     <div className="App">
-      <Router>
-        <Routes> {/* Use Routes instead of RouterProvider */}
-          <Route path="/" element={<Main />}>
-            <Route path="/" element={<PropertyList />} />
-            <Route path="propertylist/:id" element={<PropertyDetails />} />
-            <Route path="login" element={<Login />} />
-            <Route path="signup" element={<Signup />} />
-          </Route>
-        </Routes>
-      </Router>
+      <RouterProvider router={router} />
       <ToastContainer
         position='bottom-center' // Corrected position typo
         autoClose={3000}
